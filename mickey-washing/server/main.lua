c1fa9d054f487a6e307c594cd5db92016ab675c2
Marcus <30908995+mickeystix@users.noreply.github.com>
2021-07-21 03:15:09 -0500

    Update main.lua

--- mickey-washing/server/main.lua
+++ mickey-washing/server/main.lua
@@ -1,5 +1,5 @@
-RepentzFW = nil
-TriggerEvent('RepentzFW:GetObject', function(obj) RepentzFW = obj end)
+QBCore = nil
+TriggerEvent('QBCore:GetObject', function(obj) QBCore = obj end)
 
 local ItemList = Config.ItemList
 
@@ -9,13 +9,13 @@ RegisterServerEvent("mickey-washing:server:tradeItems")
 AddEventHandler("mickey-washing:server:tradeItems", function(sellPrice)
     local src = source
     local price = 0
-    local Player = RepentzFW.Functions.GetPlayer(src)
+    local Player = QBCore.Functions.GetPlayer(src)
     if Player.PlayerData.items ~= nil and next(Player.PlayerData.items) ~= nil then 
         for k, v in pairs(Player.PlayerData.items) do 
             if Player.PlayerData.items[k] ~= nil then 
                 if ItemList[Player.PlayerData.items[k].name] ~= nil then 
                     price = price + (ItemList[Player.PlayerData.items[k].name] * Player.PlayerData.items[k].amount)
-                    local item = RepentzFW.Shared.Items[Player.PlayerData.items[k].name]
+                    local item = QBCore.Shared.Items[Player.PlayerData.items[k].name]
                     Player.Functions.RemoveItem(Player.PlayerData.items[k].name, Player.PlayerData.items[k].amount, k)
                     TriggerClientEvent('inventory:client:ItemBox', source, item, "remove")
                 end
@@ -30,9 +30,9 @@ end)
 
 
 
-RepentzFW.Functions.CreateCallback('mickey-washing:server:getSellPrice', function(source, cb)
+QBCore.Functions.CreateCallback('mickey-washing:server:getSellPrice', function(source, cb)
     local retval = 0
-    local Player = RepentzFW.Functions.GetPlayer(source)
+    local Player = QBCore.Functions.GetPlayer(source)
     if Player.PlayerData.items ~= nil and next(Player.PlayerData.items) ~= nil then 
         for k, v in pairs(Player.PlayerData.items) do 
             if Player.PlayerData.items[k] ~= nil then 
@@ -47,9 +47,9 @@ RepentzFW.Functions.CreateCallback('mickey-washing:server:getSellPrice', functio
 end)
 
 --Change this over to check for weight
-RepentzFW.Functions.CreateCallback('mickey-washing:server:checkWeight', function(source, cb)
+QBCore.Functions.CreateCallback('mickey-washing:server:checkWeight', function(source, cb)
     local retval = 0
-    local Player = RepentzFW.Functions.GetPlayer(source)
+    local Player = QBCore.Functions.GetPlayer(source)
     if Player.PlayerData.items ~= nil and next(Player.PlayerData.items) ~= nil then 
         for k, v in pairs(Player.PlayerData.items) do 
             if Player.PlayerData.items[k] ~= nil then 
